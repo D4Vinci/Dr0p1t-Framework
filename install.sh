97dc0b0ee981cd12f41ce2b5d2aaf4557436c3ca
mrpnkt <mr@mrpnkt.com>
2017-07-27 13:53:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install wine32
 sudo winecfg
+sudo wget https://www.python.org/ftp/python/2.7.1/python-2.7.1.msi
 sudo wine msiexec /i python-2.7.12.msi /L*v log.txt
 sudo wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
